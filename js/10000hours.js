5f037ebdc6acd3d81ce81229fbae7040e838eefe
junsikBang <143987702+junsikBang@users.noreply.github.com>
2023-09-05 16:09:50 +0900

    변수 선언 방식 변경

--- js/10000hours.js
+++ js/10000hours.js
@@ -8,18 +8,18 @@ const loading = document.querySelector('.result_loading');
 
 function calculator() {
     const fieldValue = document.querySelector('#field_value');
-    let timeValue = document.querySelector('#time_value');
-    let timeValue_int = Number(timeValue.value);
+    const timeValue = document.querySelector('#time_value');
+    const timeValue_int = Number(timeValue.value);
 
     const fieldResult = document.querySelector('.field_result');
     const timeResult = document.querySelector('.time_result');
 
     if(fieldValue.value == ""){
-        alert('입력되지 않았습니다.');
+        alert('분야가 입력되지 않았습니다.');
         fieldValue.focus();
         return false;
     } else if(timeValue.value == "") {
-        alert('입력되지 않았습니다.');
+        alert('시간이 입력되지 않았습니다.');
         timeValue.focus();
         return false;
     } else if(timeValue_int > 24) {
@@ -32,9 +32,9 @@ function calculator() {
 
     setTimeout(function(){
         loading.style.display = 'none';
-        result.style.display = 'flex';
         fieldResult.innerText = fieldValue.value;
         timeResult.innerText = parseInt((10000/timeValue_int), 10);
+        result.style.display = 'flex';
     }, 1800);
 }
 
